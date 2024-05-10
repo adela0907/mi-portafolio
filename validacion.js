1467f6654309c03e739f846c6778efbaddab265d
Adela Olivera <157389359+adela0907@users.noreply.github.com>
2024-05-10 19:21:07 -0300

    portafolio-v3

--- validacion.js
+++ validacion.js
@@ -1,1 +1,36 @@
-//Haz tú validación en javascript acá
+const campoDeFormulario= document.querySelectorAll("[required]");
+const error = document.querySelector('#error')
+
+
+function copyText() {
+    const inputText = document.getElementById("myInput");
+
+    inputText.select();
+    inputText.setSelectionRange(0,99999);
+    
+    navigator.clipboard.writeText(inputText.value)
+}
+
+//recorremos los campos con forEach que recibe una funcion flecha por parametro 
+campoDeFormulario.forEach((campo)=>{
+    campo.addEventListener("blur", ()=> verificarCampo(campo))
+})
+function mensajeError() { 
+    console.log("Error.");
+    error.textContent = "Complete todos los campos"
+ } 
+function verificarCampo(campo){
+    if(campo.name=== campo.value.length <= 2){
+        campo.style.borderColor = "red";
+        mensajeError()
+    }
+    else{
+        error.remove('mensajeError')
+    };
+
+  
+    console.log("verifica", campo.name);
+
+}
+
+
